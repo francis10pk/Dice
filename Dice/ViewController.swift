dfe9a0adf562819ff2ccbc72e6be1a8fbc6d4ddc
Zahra <zahrs@gmsil.com>
2024-01-22 17:42:32 -0500

    Initial Commit

---
+++ Dice/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  Dice
+//
+//  Created by english on 2024-01-22.
+//
+
+import UIKit
+import Foundation
+class ViewController: UIViewController {
+
+    var leftDiceIndex = 0
+    var rightDiceIndex = 5
+    let imageArray = [UIImage(named: "DiceOne"),UIImage(named: "DiceTwo"),
+                     UIImage(named: "DiceThree"),UIImage(named: "DiceFour"),
+                     UIImage(named: "DiceFive"),UIImage(named: "DiceSix")]
+    
+    @IBOutlet weak var ibimage01: UIImageView!
+    @IBOutlet weak var ibimage02: UIImageView!
+    
+    @IBAction func rollDice(_ sender: Any) {
+        ibimage01.image = imageArray[leftDiceIndex]
+        ibimage02.image = imageArray[rightDiceIndex]
+        //leftDiceIndex = leftDiceIndex + 1
+        //rightDiceIndex = rightDiceIndex - 1
+        leftDiceIndex = Int.random(in: 0..<6)
+        rightDiceIndex = Int.random(in: 0..<6)
+        /*if (leftDiceIndex >= 6)
+        {
+            leftDiceIndex = 0
+        }
+        
+        if (rightDiceIndex < 0)
+        {
+            rightDiceIndex = 5
+        }*/
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //ibimage01.image = UIImage(named: "DiceTwo")
+        //ibimage02.image = UIImage(named: "DiceThree")
+        /*ibimage01.image = [UIImage(named: "DiceOne"),UIImage(named: "DiceTwo"),
+                           UIImage(named: "DiceThree"),UIImage(named: "DiceFour"),
+                           UIImage(named: "DiceFive"),UIImage(named: "DiceSix")][0]
+        ibimage02.image = [UIImage(named: "DiceOne"),UIImage(named: "DiceTwo"),
+                           UIImage(named: "DiceThree"),UIImage(named: "DiceFour"),
+                           UIImage(named: "DiceFive"),UIImage(named: "DiceSix")][5]
+         */
+    }
+
+
+}
+
